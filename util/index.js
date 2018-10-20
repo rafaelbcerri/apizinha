0579461974fb5e2fc21413876248a49965264a47
Rafael Cerri <rafaelbcerri@gmail.com>
2018-10-20 15:21:30 -0300

    Rollback util function

--- util/index.js
+++ util/index.js
@@ -1,6 +1,10 @@
 function isAllowedPath(path) {
-  const allowedPaths = {"/login":true, "/api-docs":true, "/signup":true, "/conflict":true};
-  return allowedPaths[path];
+  const allowedPaths = ["/login", "/api-docs", "/signup", "/conflict"];
+  const pathIndex = allowedPaths.findIndex((allowedPath) => (
+    path.includes(allowedPath)
+  ));
+
+  return pathIndex >= 0;
 }
 
 module.exports = {
